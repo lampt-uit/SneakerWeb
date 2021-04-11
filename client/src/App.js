8945d5b4c2f54416cf9627847307c615df520589
Pham Tan Lam <lamlampnqn12@gmail.com>
2021-04-11 20:55:30 +0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import MainPages from './components/mainpages/Pages';
+
+function App() {
+	return (
+		<div className='App'>
+			<Header />
+			<MainPages />
+			<Footer />
+		</div>
+	);
+}
+
+export default App;
