a5d4eca325f59db3b82dbb72af601bd723c192f1
KimThang <18521388@gm.uit.edu.vn>
2021-05-10 19:57:53 +0700

    loading

---
+++ client/src/components/mainpages/utils/Loading/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Loading.css'
+function Loading() {
+    return (
+        <div className='loading-area'>
+            <div className="loader">Loading...</div>
+            <div className="load_anim1"></div>
+            <div className="load_anim2"></div>
+        </div>
+    )
+}
+
+export default Loading
